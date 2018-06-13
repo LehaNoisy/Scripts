98cb33d0a0d3ededf1e42b3a1f916ab0a41cdeab
Alexey <shumilovy@mail.ru>
2018-06-13 12:43:05 +0300

    Update labda-script-create-bucket.py

--- labda-script-create-bucket.py
+++ labda-script-create-bucket.py
@@ -4,10 +4,10 @@ def lambda_handler(event, context):
     myS3 = boto3.client('s3')
     try:
         result = myS3.list_buckets()
-        print(result)
+        print(results)
         output = ""
         for bucket in results['Buckets']:
-            output = output + bucket['Name'] + "\n"
-        print("<h1><font color=green> S3 Bucket List:</font></h1><br><br>" + output)
+            output = output + bucket['Name'] + "<br>"
+        return("<h1><font color=green> S3 Bucket List:</font></h1><br><br>" + output)
     except:
-        print("<h1><font color=red>Error!</font></h1><br><br>")
+        return("<h1><font color=red>Error!</font></h1><br><br>")
